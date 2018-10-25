2d6f01d923f7effe1c4909d2fdd4251eea0c1cd2
Lindsay Koprivica <lindsayeplin@gmail.com>
2018-10-25 23:05:38 +0800

    checkpoint-send-messages updated/completed

--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -7,26 +7,18 @@ class MessageList extends Component {
         super(props)
         this.state = {
             messages: [],
-<<<<<<< HEAD
-            value: ""
-=======
             content: "",
             userName: "",
             sentAt: "",
             roomId: ""
->>>>>>> checkpoint-send-messages
         }
         this.messagesRef = this.props.firebase.database().ref('messages')
     }
 
     componentDidMount() {
         this.messagesRef.on('child_added', snapshot => {
-<<<<<<< HEAD
-            temp.push(snapshot.val())
-=======
             const message = snapshot.val();
             message.key = snapshot.key;
->>>>>>> checkpoint-send-messages
             this.setState({
                 messages: this.state.messages.concat(message)
             })
@@ -34,20 +26,6 @@ class MessageList extends Component {
         });
     }
 
-<<<<<<< HEAD
-    handleChange=(event)=>{
-      this.setState({value: event.target.value});
-    }
-
-    handleSubmit=(event)=>{
-      event.preventDefault();
-      this.messagesRef.push({
-        message: this.state.value
-      });
-      this.setState({
-        value: ""
-      })
-=======
     createMessage=(event)=> {
         var d = new Date();
         var n = d.toLocaleTimeString();
@@ -81,27 +59,21 @@ class MessageList extends Component {
         sentAt: "",
         roomId: this.props.activeRoom.id
       });
->>>>>>> checkpoint-send-messages
     }
 
     render() {
       console.log(this.props.activeRoom + " from app");
       return (
-<<<<<<< HEAD
-        <section className = "MessageList">
-=======
         <section>
         <div className="chatroom-name">
           <h2>{this.props.activeRoom.name}</h2>
         </div>
         <div className = "MessageList">
->>>>>>> checkpoint-send-messages
           <ul>
              {this.state.messages.filter(val => {
                 return this.props.activeRoom.key === val.roomId
             })
             .map((val, index) => {
-
                 return <li className ="message-form" key={index}>
                   {val.username}
                   {val.content}
@@ -111,15 +83,6 @@ class MessageList extends Component {
             }
           </ul>
 
-<<<<<<< HEAD
-          <form onSubmit={this.handleSubmit}>
-           <label>
-             Create Message:
-             <input type="text" value={this.state.value} onChange={this.handleChange} />
-           </label>
-           <input type="submit" value="Submit" />
-         </form>
-=======
         <div className="newMessage">
           <form className="message-form" onSubmit={(event)=>this.createMessage(event)}>
             <div>
@@ -130,7 +93,6 @@ class MessageList extends Component {
         </div>
 
         </div>
->>>>>>> checkpoint-send-messages
         </section>
       );
     }
